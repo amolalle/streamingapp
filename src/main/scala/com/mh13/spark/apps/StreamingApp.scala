c5e0aecf9e9cbb726ba4b107ed824138092179c7
amolalle <amol.alle@gmail.com>
2018-08-06 21:42:57 -0400

    moved to package

---
+++ src/main/scala/com/mh13/spark/apps/StreamingApp.scala
@@ -0,0 +1,28 @@
+package com.mh13.spark.apps
+
+import kafka.serializer.StringDecoder
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object KafkaSparkStreamingApp {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setAppName("KafkaSparkStreamingApp")
+    sparkConf.setIfMissing("spark.master", "local[*]")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+
+    val topicsToListen = Set[String] {
+      "my_topic"
+    }
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> "127.0.01:9092")
+
+    val kafkaStream = KafkaUtils.createDirectStream[String,String,StringDecoder,StringDecoder](ssc,kafkaParams,topicsToListen)
+
+    kafkaStream.print
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+}
